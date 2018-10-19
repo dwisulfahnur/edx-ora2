55a118140c5536d1af517d99aa0c95530539a1e3
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2018-10-19 18:54:44 +0700

    Add minio as backend of fileupload

--- openassessment/fileupload/backends/__init__.py
+++ openassessment/fileupload/backends/__init__.py
@@ -17,5 +17,7 @@ def get_backend():
         return swift.Backend()
     elif backend_setting == "django":
         return django_storage.Backend()
+    elif backend_setting == "minio":
+        return minio.Backend()
     else:
         raise ValueError("Invalid ORA2_FILEUPLOAD_BACKEND setting value: %s" % backend_setting)
